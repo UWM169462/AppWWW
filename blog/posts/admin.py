72c267fbc4a13e992b5270fe99e63387805c466d
UWM169462 <uwm169462@wp.pl>
2025-10-23 17:48:55 +0200

    Lab3

--- blog/posts/admin.py
+++ blog/posts/admin.py
@@ -1,10 +1,34 @@
 from django.contrib import admin
+from .models import Category, Topic, Post
 
-# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
 
-# modele musimy zaimportować
-from .models import Category, Topic
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category']
 
-# a następnie zarejestrować (pokazano najprostszy przypadek)
-admin.site.register(Category)
-admin.site.register(Topic)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'topic_category', 'created_at', 'created_by']
+    readonly_fields = ['created_at']
+    list_filter = ['topic__category', 'topic', 'created_by']
+    prepopulated_fields = {"slug": ("title",)}
+
+    @admin.display(description="Topic (Category)")
+    def topic_category(self, obj):
+        return f"{obj.topic.name} {obj.topic.category.name}"
+    topic_category.admin_order_field = 'topic__name'
+    topic_category.short_description = 'Topic (Category)'
+
+
+
+# # dla przykładowej klasy
+# class PersonAdmin(admin.ModelAdmin):
+#     # zmienna list_display przechowuje listę pól, które mają się wyświetlać w widoku listy danego modelu w panelu administracynym
+#     list_display = ['name', 'shirt_size']
+#
+# # ten obiekt też trzeba zarejestrować w module admin
+# admin.site.register(Person, PersonAdmin)
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Topic, TopicAdmin)
--- blog/posts/models.py
+++ blog/posts/models.py
@@ -1,12 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=60)
     description = models.TextField()
-
+    class Meta:
+        ordering = ['name']
+    def __str__(self):
+        return self.name
 
 class Topic(models.Model):
     name = models.CharField(max_length=60)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        ordering = ['name']
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    text = models.TextField()
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    slug = models.SlugField(unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    class Meta:
+        ordering = ['-created_at']
+    def __str__(self):
+        return " ".join(self.text.split()[:5]) + ("..." if len(self.text.split()) > 5 else "")
